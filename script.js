981ef326ea9e259cf228092e9783d9e792bc5b27
ajacobsen <atze.danjo@gmail.com>
2022-03-01 02:08:29 +0100

    Allow to input decimal numbers

--- script.js
+++ script.js
@@ -41,12 +41,14 @@ const digits = document.querySelectorAll('input[data-type="digit"]');
 const operators = document.querySelectorAll('input[data-type="operator"]');
 const AC = document.querySelector('input[data-type="ac"]');
 const equals = document.querySelector('input[data-type="equals"]');
+const point = document.querySelector('input[data-type="point"]');
 
 let prevDisplayValue;
 let displayValue;
 let operator;
 let firstInput;
 let lastInputType;
+let hasPoint;
 
 function clearDisplay(ev) {
     display.value = '0';
@@ -55,18 +57,21 @@ function clearDisplay(ev) {
     operator = null;
     firstInput = true;
     lastInputType = null;
+    hasPoint = false;
 }
 
 function digitInput(ev) {
-    lastInputType = 'digit';
     if (firstInput) {
         displayValue = this.value;
         display.value = displayValue;
         firstInput = false;
+        lastInputType = 'digit';
         return;
     }
+
     displayValue += this.value;
     display.value = displayValue
+    lastInputType = 'digit';
 }
 
 function operatorInput(ev) {
@@ -82,17 +87,33 @@ function operatorInput(ev) {
     operator = this.value;
     firstInput = true;
     lastInputType = 'operator';
+    hasPoint = false;
 }
 
 function equalsInput(ev) {
     displayValue = operate(operator, prevDisplayValue, displayValue);
     display.value = displayValue;
     prevDisplayValue = null;
+    hasPoint = false;
 }
 
+function pointInput(ev) {
+    if (lastInputType === 'point' || hasPoint) return;
+
+    if (firstInput) {
+        displayValue = '0.';
+    } else {
+        displayValue += '.';
+    }
+    display.value = displayValue;
+    firstInput = false;
+    lastInputType = 'point';
+    hasPoint = true;
+}
 
 AC.addEventListener('click', clearDisplay);
 equals.addEventListener('click', equalsInput);
+point.addEventListener('click', pointInput);
 digits.forEach(digit => {
     digit.addEventListener('click', digitInput);
 });
